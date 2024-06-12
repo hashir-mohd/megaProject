7778234ca1b798f989dd33492f9d7c9e0db25ed8
one <esport.py.m45@gmail.com>
2024-06-12 16:33:04 +0530

    added user controller functionality

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,66 @@
 import {asyncHandler} from '../utils/asyncHandler.js';
+import {ApiError} from '../utils/apiError.js';
+import {User} from '../models/user.model.js';
+import {uploadOnCloudinary} from '../utils/cloudinary.js';
+import {ApiResponse} from '../utils/ApiResponse.js';
+
 
 const registerUser = asyncHandler(async (req,res)=>{
-    res.status(200).json({
-        message : "ok"
+    const {userName,fullname,email,password} = req.body;
+    console.log("email: ", email);
+
+    if (
+        [fullname,email,password,userName].some((field)=>
+        field?.trim()==="")
+    ) {
+        throw new ApiError(400,"All field are required");
+    }
+
+    const existedUser = User.findOne({
+        $or:[{ username}, { email}]
     })
+    if (existedUser) {
+        throw new ApiError(400,"User already exists");
+    }
+
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    if(!avatarLocalPath){
+        throw new ApiError(400,"Avatar is required");
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath) ;
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath) ;
+
+    if(!avatar){
+        throw new ApiError(400,"Avatar is required");
+    }
+
+    const user = await User.create({
+        fullname,
+        avatar: avatar.url,
+        coverImage: coverImage.url || "",
+        email,
+        password,
+        userName.tolowerCase() 
+
+    })
+
+    const createdUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+
+    if(!createdUser){
+        throw new ApiError(400,"Something went wrong while creating the user");
+    }
+
+    return res.status(201).json(
+        new ApiResponse(
+            201,
+            createdUser,
+            "User created successfully"
+        )
+    )
 })
 
 export {registerUser}
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken"
 import bcrypt from "bcrypt"
 
 const userSchema = new Schema({
-    username:{
+    userName:{
         type: String,
         required : true,
         unique : true,
@@ -50,7 +50,7 @@ const userSchema = new Schema({
 
 userSchema.pre("save",async function (next){
     if(!this.isModofied("password")) return next();
-    this.password = bcrypt.hash(this.password,10)
+    this.password = await bcrypt.hash(this.password,10)
     next()
 })
 
@@ -63,7 +63,7 @@ userSchema.methods.generateAccessToken = function(){
         {
             _id : this._id,
             email : this.email,
-            username : this.username,
+            username : this.userName,
             fullname : this.fullname
         },
         process.env.ACCESS_TOKEN_SECRET,
